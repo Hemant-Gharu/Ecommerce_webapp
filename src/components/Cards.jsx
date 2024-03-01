1ec8eaaf229bb5ca158a8b54a1346fa6f3e0c214
Hemant Gharu <140576554+Hemant-Gharu@users.noreply.github.com>
2024-03-01 05:01:01 +0000

    images added

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,12 +1,12 @@
 import styles from "../styles/cards.module.css";
-// import img from "../assets/macbook_air.jpg/";
+import img from "../assets/macbook_air.jpg/";
 
 const Cards = ({ item }) => {
   const { product, price } = item;
   return (
     <div className={styles.cards}>
       <div className={styles.card}>
-        <img src="../assets/macbook_air.jpg" alt="" className={styles.image} />
+        <img src={img} alt="" className={styles.image} />
         <p className={styles.product}>{product}</p>
         <p className={styles.price}>{price}</p>
       </div>
